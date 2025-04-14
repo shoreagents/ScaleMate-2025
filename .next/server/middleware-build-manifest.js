ea46dbbe60f275e30656572d1fd4b957d68ccfd0
ShoreAgents <dev.shoreagents@gmail.com>
2025-04-14 10:17:18 +0800

    new admin cards

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -29,11 +29,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/admin/dashboard.js"
-    ],
-    "/test-dashboard": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/test-dashboard.js"
     ]
   },
   "ampFirstPages": []
