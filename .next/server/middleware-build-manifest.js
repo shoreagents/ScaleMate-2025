69b55e8eba28c0ccb4ee0ff82fcb6516dbdcb042
shoreagents <dev.shoreagents@gmail.com>
2025-04-15 11:05:27 +0800

    added quote tool and readiness

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -25,10 +25,10 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/admin": [
+    "/readiness": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/admin.js"
+      "static/chunks/pages/readiness.js"
     ]
   },
   "ampFirstPages": []
