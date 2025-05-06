35ed0b8b0d9b431da8a6506bf9d03d3b84d03595
shoreagents <dev.shoreagents@gmail.com>
2025-05-06 14:47:30 +0800

    fix button styling inconsistency

--- src/pages/tools/components/FinalCTA.tsx
+++ src/pages/tools/components/FinalCTA.tsx
@@ -67,9 +67,11 @@ const PrimaryButton = styled.button`
   width: 280px;
   border: none;
   cursor: pointer;
+  font-size: 1rem;
+  min-width: 280px;
 
   @media (min-width: 768px) {
-    width: auto;
+    width: 280px;
   }
 
   &:hover {
@@ -90,9 +92,11 @@ const SecondaryButton = styled(Link)`
   transition: background-color 0.2s;
   justify-content: center;
   width: 280px;
+  font-size: 1rem;
+  min-width: 280px;
 
   @media (min-width: 768px) {
-    width: auto;
+    width: 280px;
   }
 
   &:hover {
